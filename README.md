6eb69d1b2397bc8b5cc8c241d6eb164114d5f2ad
Martin Lara <71472029+Garuda127@users.noreply.github.com>
2023-07-02 00:33:38 -0600

    final

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 A new Flutter project.
 
 ## Getting Started
+### Esto es un reto de esta imagen
+![alt](assets\imgs\idea.jpg)
 
+#### Resultado
+![alt](assets\imgs\Resultado.gif)
 This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
--- lib/helpers/helpers.dart
+++ lib/helpers/helpers.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as services;
 
 void cambiarStatusLight() {
-  services.SystemChrome.setSystemUIOverlayStyle(
-      services.SystemUiOverlayStyle.light);
+  services.SystemChrome.setSystemUIOverlayStyle(services
+      .SystemUiOverlayStyle.light
+      .copyWith(statusBarColor: Colors.transparent));
 }
 
 void cambiarStatusDark() {
-  services.SystemChrome.setSystemUIOverlayStyle(
-      services.SystemUiOverlayStyle.dark);
+  services.SystemChrome.setSystemUIOverlayStyle(services
+      .SystemUiOverlayStyle.dark
+      .copyWith(statusBarColor: Colors.transparent));
 }
